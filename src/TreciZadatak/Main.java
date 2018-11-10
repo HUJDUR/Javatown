788c3aa60f4e530015cc8bfc85e6e6f5b3c95dff
Nadir Hujdur <nadir.hujdur2002@gmail.com>
2018-11-10 23:24:00 +0100

    Drugi zadatak

---
+++ src/TreciZadatak/Main.java
@@ -0,0 +1,22 @@
+package TreciZadatak;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import PrviZadatak.Maratonac;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Maratonac.rastavljanjeMaratonaca();
+		
+		Scanner input = new Scanner(System.in);
+		String unos = input.next();
+		
+		
+		
+		
+	}
+
+}
