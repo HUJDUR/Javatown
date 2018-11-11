77deaf29a187e274f145118a6ca10836301c012e
Nadir Hujdur <nadir.hujdur2002@gmail.com>
2018-11-11 13:26:43 +0100

    Final

---
+++ src/PetiZadatak/MainZaPeti.java
@@ -0,0 +1,17 @@
+ package PetiZadatak;
+
+import java.io.IOException;
+
+import PrviZadatak.Maratonac;
+
+public class MainZaPeti {
+
+	public static void main(String[] args) throws IOException {
+		
+		Maratonac.rastavljanjeUcesnika();
+		
+		Maratonac.najbrziUcesnici();
+		
+	}
+
+}
